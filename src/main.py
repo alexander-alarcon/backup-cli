36ebbcf1a117be1d0b57b85d66569996b6a9d201
Alexander Alarcon <walarcon14@gmail.com>
2023-10-07 12:32:05 -0500

    feat(cli): Add -bs/--backup-strategy option with validation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
 import click
 
-from src.backup.backup import Backup
-from src.logger import ColoredLogger, LogLevel
+from .backup.backup import Backup
+from .backup.backup_factory import BackupFactory
+from .logger import ColoredLogger, LogLevel
+from .type_definitions import BackupArgs, BackupStrategy
+
+
+def validate_backup_strategy(
+    ctx: click.Context, param: click.Parameter, value: Optional[str]
+) -> BackupStrategy:
+    """
+    Validate the provided backup strategy.
+
+    Args:
+        ctx (click.Context): The click context object.
+        param (click.Parameter): The click parameter object.
+        value (Optional[str]): The value of the backup strategy.
+
+    Returns:
+        BackupStrategy: The validated backup strategy.
+
+    Raises:
+        click.BadParameter: If the backup strategy is invalid.
+    """
+    if value is None:
+        return BackupStrategy.INCREMENTAL
+    try:
+        return BackupStrategy(value)
+    except ValueError:
+        raise click.BadParameter(
+            f"Invalid backup strategy '{value}'. Please use 'incremental' or 'full'."
+        )
 
 
 @click.command()
@@ -22,10 +51,20 @@ from src.logger import ColoredLogger, LogLevel
     required=True,
     help="Destination directory to backup.",
 )
+@click.option(
+    "-bs",
+    "--backup-strategy",
+    type=click.Choice(
+        [strategy.value for strategy in BackupStrategy], case_sensitive=False
+    ),
+    callback=validate_backup_strategy,
+    help="Choose a backup strategy: incremental, etc.",
+    show_default=True,
+    show_choices=True,
+    default=BackupStrategy.INCREMENTAL.value,
+)
 def main(
-    verbose: bool,
-    source: str,
-    destination: str,
+    verbose: bool, source: str, destination: str, backup_strategy: BackupStrategy
 ) -> None:
     """Backup CLI."""
     logger = ColoredLogger(name="main", verbose=verbose)
