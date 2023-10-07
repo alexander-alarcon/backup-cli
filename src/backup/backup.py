af0c3329516385dcb2788b3828d900323cd2a0bb
Alexander Alarcon <walarcon14@gmail.com>
2023-10-07 11:55:42 -0500

    feat(Backup): Introduce Backup as a base class for backup strategies

--- src/backup/backup.py
+++ src/backup/backup.py
@@ -1,148 +1,53 @@
-# src/backup_logic.py (Backup Logic Module)
-
-import os
 import pathlib
-import shutil
+from abc import ABC, abstractmethod
 
-from src.logger import ColoredLogger, LogLevel
+from src.logger import ColoredLogger
 
 
-class Backup:
+class Backup(ABC):
     def __init__(
         self, source_directory: str, destination_directory: str, logger: ColoredLogger
     ) -> None:
         """
-        Initializes a new instance of the class.
+        Initializes a new instance of the Backup class, which serves as an abstract base
+        class for various backup operations. This class defines the common attributes and
+        behavior shared by different types of backups.
 
         Args:
-            source_directory (str): The source directory.
-            destination_directory (str): The destination directory.
-            logger (ColoredLogger): The logger instance.
+            source_directory (str): The source directory containing data to be backed up.
+            destination_directory (str): The destination directory where the backup data
+                will be stored.
+            logger (ColoredLogger): The logger instance used for recording backup
+                progress and any related messages.
 
-        Returns:
-            None
+        Attributes:
+            source_directory (pathlib.Path): A pathlib.Path object representing the path to
+                the source directory.
+            destination_directory (pathlib.Path): A pathlib.Path object representing the
+                path to the destination directory.
+            logger (ColoredLogger): The logger instance used for logging backup operations.
+
+        Raises:
+            NotImplementedError: This class is intended to be subclassed, so attempting
+                to create an instance of this abstract base class will raise an error.
+
         """
         self.source_directory = pathlib.Path(source_directory)
         self.destination_directory = pathlib.Path(destination_directory)
         self.logger: ColoredLogger = logger
 
-    def create_directory(self, directory: pathlib.Path) -> None:
-        """
-        Create a directory.
-
-        Args:
-            directory (pathlib.Path): The path of the directory to be created.
-
-        Returns:
-            None
-
-        Raises:
-            OSError: If the creation of the directory fails.
-
-        """
-        try:
-            directory.mkdir(parents=True, exist_ok=True)
-        except OSError:
-            self.logger.log(
-                LogLevel.ERROR, f"Creation of the directory {directory} failed"
-            )
-
-    def copy_file(
-        self, source_file: pathlib.Path, destination_file_path: pathlib.Path
-    ) -> None:
-        """
-        Copies a file from the source path to the destination path.
-
-        Args:
-            source_file (pathlib.Path): The path to the source file.
-            destination_file_path (pathlib.Path): The path to the destination file.
-
-        Returns:
-            None
-        """
-        if not source_file.exists():
-            self.logger.log(
-                LogLevel.ERROR, f"[ERROR] Source file {source_file} does not exist."
-            )
-            return
-
-        if source_file.is_symlink():
-            link_target: str = os.readlink(source_file)
-            try:
-                os.symlink(link_target, destination_file_path)
-                self.logger.log(
-                    LogLevel.COPY,
-                    f"{source_file} (symlink) -> {destination_file_path}",
-                )
-            except FileExistsError:
-                self.logger.log(
-                    LogLevel.SKIP,
-                    f"Symbolic link {source_file} already exists at destination.",
-                )
-        elif source_file.is_file():
-            # If the source file is a regular file, copy it
-            if not destination_file_path.exists() or (
-                source_file.stat().st_size != destination_file_path.stat().st_size
-                or source_file.stat().st_mtime != destination_file_path.stat().st_mtime
-            ):
-                shutil.copy2(source_file, destination_file_path)
-                self.logger.log(
-                    LogLevel.COPY, f"{source_file} -> {destination_file_path}"
-                )
-            else:
-                self.logger.log(LogLevel.SKIP, f"File {source_file} is unchanged.")
-
-    def copy_files(
-        self, source_directory: pathlib.Path, destination_directory: pathlib.Path
-    ) -> None:
-        """
-        Copy files from the source directory to the destination directory recursively.
-
-        Parameters:
-            source_directory (pathlib.Path): The path to the source directory.
-            destination_directory (pathlib.Path): The path to the destination directory.
-
-        Returns:
-            None: This function does not return anything.
-        """
-        self.logger.log(
-            LogLevel.INFO,
-            f"Copying from: {self.source_directory} -> to: {self.destination_directory}",
-        )
-
-        source_dir_path: pathlib.Path = source_directory
-        destination_dir_path: pathlib.Path = destination_directory
-
-        self.create_directory(destination_dir_path)
-
-        for source_file in source_dir_path.iterdir():
-            destination_file_path: pathlib.Path = (
-                destination_dir_path / source_file.name
-            )
-            self.copy_file(
-                source_file=source_file,
-                destination_file_path=destination_file_path,
-            )
-
-            if source_file.is_dir():
-                subdirectory: pathlib.Path = source_dir_path / source_file.name
-                self.copy_files(
-                    source_directory=subdirectory,
-                    destination_directory=destination_file_path,
-                )
-
+    @abstractmethod
     def perform_backup(self) -> None:
         """
-        Performs a backup by copying files from the source directory to the destination directory.
+        Perform a backup operation.
 
-        Parameters:
-            None.
+        This method should be implemented in subclasses to define the specific logic
+        and steps required to perform a backup. Subclasses should override this method
+        to provide backup functionality tailored to their particular use case.
 
-        Returns:
-            None.
+        Raises:
+            NotImplementedError: Subclasses must implement this method to define the
+                backup procedure.
+
         """
-        self.create_directory(self.destination_directory)
-        self.copy_files(
-            source_directory=self.source_directory,
-            destination_directory=self.destination_directory,
-        )
+        raise NotImplementedError("perform_backup must be implemented.")
